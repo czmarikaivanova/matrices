70a2a39ef78da4c069c6db7ea7b77b6cab83b164
marika <marikaivanova@seznam.cz>
2016-07-23 02:27:01 +0200

    indent

--- src/com/uib/matrices/Matrix.java
+++ src/com/uib/matrices/Matrix.java
@@ -3,8 +3,13 @@ package com.uib.matrices;
 public class Matrix<T> {
 	private T[][] entries;
 
-	public void set(T[][] entries) { this.entries = entries; }
-    public T[][] get() { return entries; }
+	public void set(T[][] entries) { 
+		this.entries = entries; 
+	}
+	
+    public T[][] get() { 
+    	return entries; 
+	}
 	
 	
 }
